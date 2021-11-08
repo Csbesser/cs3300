b54e898316935b2a985c630137772091c605ab44
root <calebsbesser@yahoo.com>
2021-11-08 05:58:19 +0000

    Iteration4

---
+++ Project Model/Iteration_4/model_specs.rb
@@ -0,0 +1,20 @@
+#example taken from website provided
+
+require "rails_helper"
+
+RSpec.describe Post, :type => :model do
+  context "with 2 or more comments" do
+    it "orders them in reverse chronologically" do
+      post = Post.create!
+      comment1 = post.comments.create!(:body => "first comment")
+      comment2 = post.comments.create!(:body => "second comment")
+      expect(post.reload.comments).to eq([comment2, comment1])
+    end
+  end
+end
+
+
+#this is test for the rspec
+#this is also a test for the rspec
+
+
