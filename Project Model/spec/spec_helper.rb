293210ef7d1a9994b333da927ca0246c7f0cb58d
root <calebsbesser@yahoo.com>
2021-11-15 20:47:48 +0000

    Cl stuff

--- Project Model/spec/spec_helper.rb
+++ Project Model/spec/spec_helper.rb
@@ -95,6 +95,26 @@ RSpec.configure do |config|
   # Setting this allows you to use `--seed` to deterministically reproduce
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
-  Kernel.srand config.seed
+  Kernel.srand config.seed'
+
+
+  
+ 
 =end
+
+#simplecov
+require 'simplecov'
+
+SimpleCov.start
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
 end
